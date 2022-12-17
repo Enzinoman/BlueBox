4d3b1a9b3006458f319abef7cebd1a4b508df490
Raymond Patterson <enzinoman@gmail.com>
2022-12-16 19:55:13 -0700

    header update

--- vidRental/src/App.jsx
+++ vidRental/src/App.jsx
@@ -2,33 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/header'
-<<<<<<< Updated upstream
-=======
-import DisplayArea from './components/DisplayArea'
->>>>>>> Stashed changes
+
 // import './components/banner';
 // import './components/navbar';
 
 
 function App() {
-<<<<<<< Updated upstream
-  const [count, setCount] = useState(0)
-  
-  
-  
-=======
->>>>>>> Stashed changes
+
 
   return (
     <div className="App">
       <Header />
-<<<<<<< Updated upstream
-      
-=======
 
-      <DisplayArea/>
-
->>>>>>> Stashed changes
     </div>
   )
 }
