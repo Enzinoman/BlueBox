b12da8467fc2f377bc77cf2e7b6afcaf549e2360
Raymond Patterson <enzinoman@gmail.com>
2022-12-16 19:57:07 -0700

    header update 3

--- vidRental/src/App.jsx
+++ vidRental/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/header'
-import Darea from './components/Darea'
+
+
+
 // import './components/banner';
 // import './components/navbar';
 
@@ -10,42 +12,12 @@ import Darea from './components/Darea'
 
 
 function App() {
-<<<<<<< HEAD
-  const [count, setCount] = useState(0)
-  
-  
-  
-=======
->>>>>>> 0a04203e1a335a8e6e3c16666d6dc2ef7d17ec69
+
 
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
-      
-=======
-      <Darea/>
-      {/* <div> 
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
->>>>>>> 0a04203e1a335a8e6e3c16666d6dc2ef7d17ec69
+
     </div>
   )
 }
